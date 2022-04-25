efec30e9f4e040c29959714798c20bb2a4b5accf
Kevin D <k.sdelatorre2@gmail.com>
2022-04-25 14:34:54 -0700

    reset function

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 // Variables
-let days = document.getElementById("daily").value; // Amount of days
 const labor = document.getElementById('LA');
 const afterHours = document.getElementById('AH');
+
 const topDisp = document.getElementById('top');
 const display = document.getElementById('bottom');
 
 // Calculate Fee's
 function dailyCalc() {
+    let days = document.getElementById("daily").value; // Amount of days
     const day = 61.60; // $61.60 is one day of storage, 56 + 10% city tax (5.60)
     const tow = 185; // $185 is the standard tow fee
     
     let daily = days * day + tow; // daily = $185 + 61.60 * however many days stored
+    console.log(days);
+    console.log(daily);
 
-    return daily; 
-    
+    return daily;
 }
-console.log(days);
-console.log(dailyCalc())
 
 // Caluclate Function
 function calculate() {
@@ -25,9 +25,11 @@ function calculate() {
 }
 
 function reset() {
-    location.reload();
-    console.log('refresh');
+    let days = document.getElementById("daily").value; // Amount of days
     
+    days.value = '';
+    display.innerHTML = ''; 
+    console.log('refresh');
 }
 
 
